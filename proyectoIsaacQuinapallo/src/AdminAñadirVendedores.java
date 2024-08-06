523ca491ba0b78126e05764eeef0e2f02c57af2a
isaacquinapallo <isaac.quinapallo@epn.edu.ec>
2024-08-05 23:54:39 -0500

    15to Commit Implementacion de Imagen en el AdminAñadirVendedores.java

--- proyectoIsaacQuinapallo/src/AdminAñadirVendedores.java
+++ proyectoIsaacQuinapallo/src/AdminAñadirVendedores.java
@@ -1,5 +1,14 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class AdminAñadirVendedores {
     public JPanel mainPanel;
+    private JButton crearNuevoVendedorButton;
+    private JTextField ClienteCedulaField1;
+    private JLabel confirmadorClientetxt;
+    private JLabel iconoAnadirCliente;
+
+    public AdminAñadirVendedores() {
+        //Codigo...
+    }
 }
--- proyectoIsaacQuinapallo/src/NuevoCliente.java
+++
@@ -1,5 +0,0 @@
-import javax.swing.*;
-
-public class NuevoCliente {
-    public JPanel mainPanel;
-}
